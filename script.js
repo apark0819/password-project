170e5bd0a5e3e4c503d40e7ed4ee4d5dbf25e1f0
apark0819 <apark0819@gmail.com>
2022-06-24 00:39:07 -0700

    password box

--- script.js
+++ script.js
@@ -10,14 +10,48 @@ var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_"];
+var passwordLength = "";
+var passwordLower;
+var passwordUpper;
+var passwordNumbers;
+var passwordSpecial;
 
 function generatePassword (){
     var passwordLength = prompt("# of characters");
     if (passwordLength <= 7 || password >= 128) {
         alert("Your password must be between 8 - 128 characters.");
         var passwordLength = prompt("How many characters would you like your password to contain?");
-        } 
+        }
+    //password options     
+    var passwordLower = confirm("Would you like to add lowercase alphabets into your password?");
+    var passwordUpper = confirm("Would you like to add an uppercase alphabets into your password?");
+    var passwordNumbers = confirm("Would you like to add numbers into your password?");
+    var passwordSpecial = confirm("Would you like to add a special characters into your password?");
+    //restarts the questions
+    if (passwordLower === false && passwordUpper === false && passwordNumbers === false & passwordSpecial === false) {
+        alert("You must choose at least one of the options to continue. Please generate a new password.")
+    }
+    var password = []
+    if (passwordLower) {
+        passwordCharacters = passwordCharacters.concat(passwordLower);
+      }
+    if (passwordUpper) {
+        password = password.concat(passwordUpper);
+      }
+        
+    if (passwordNumbers) {
+        password = password.concat(passwordNumbers);
+      }
+  
+    if (passwordSpecial) {
+        password = password.concat(passwordSpecial);
+      }
+    console.log(password);
 }
+
+
+
+
 
 
 
