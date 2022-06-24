ce9cce3f9f2f73a52786663332e1805821ef5c02
apark0819 <apark0819@gmail.com>
2022-06-23 23:52:04 -0700

    var commit

--- script.js
+++ script.js
@@ -3,6 +3,27 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_"];
+
+var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV" 
+var specialchars = "~'!@#$%^&*()-_+=.?"
+
+
+function generatePassword (){
+    var passwordLength = prompt("# of characters");
+    if (passwordLength < 8) {
+        alert ("Password must have at least 8 characters.") 
+    } else {
+        if (passwordLength > 128) {
+            alert("Password cannot be over 128 characters.")
+        }
+    }
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +35,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", writePassword);
