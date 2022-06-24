8a9d53370a3793b0d2bd63a0fd0bcf3598a03a0c
apark0819 <apark0819@gmail.com>
2022-06-24 00:16:36 -0700

    password length

--- script.js
+++ script.js
@@ -2,26 +2,24 @@
 
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
+
+//var Arrays
 var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_"];
 
-var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV" 
-var specialchars = "~'!@#$%^&*()-_+=.?"
-
-
 function generatePassword (){
     var passwordLength = prompt("# of characters");
-    if (passwordLength < 8) {
-        alert ("Password must have at least 8 characters.") 
-    } else {
-        if (passwordLength > 128) {
-            alert("Password cannot be over 128 characters.")
-        }
-    }
+    if (passwordLength <= 7 || password >= 128) {
+        alert("Your password must be between 8 - 128 characters.");
+        var passwordLength = prompt("How many characters would you like your password to contain?");
+        } 
 }
+
+
 
 
 
